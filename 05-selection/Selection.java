2eb4a22e14476d34a25152af820a013cae154e09
TiffL <lei.tiffany@gmail.com>
2015-03-07 15:36:09 -0500

    HW Assignment 5 (Updated)

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -1,32 +1,44 @@
 public class Selection{
 
-    public void divide(int[] L, int SI, int EI){
-	int[] D = new int[L.length];
-	int j = 1;
+    public int select(int[] A, int k, int l, int h){
+	int[] D = new int[A.length];
+	int j = l+1;
+	int high = h;
+	int low = l;
 
-	for (int i = 0; i < SI; i++){
-	    D[i] = L[i];
+	for (int i = 0; i < l; i++){
+	    D[i] = A[i];
 	}
-	for (int i = EI+1; i < L.length; i++){
-	    D[i] = L[i];
+	for (int i = h+1; i < A.length; i++){
+	    D[i] = A[i];
 	}
 
-	int pivot = L[SI];
+	int pivot = A[l];
 
-	while (SI < EI){
-	    if (L[j] < pivot){
-		D[SI] = L[j];
-		SI++;
+	while (l < h){
+	    if (A[j] < pivot){
+		D[l] = A[j];
+		l++;
 	    }
 	    else{
-		D[EI] = L[j];
-	        EI--;
+		D[h] = A[j];
+	        h--;
 	    }
 	    j++;
 	}
 	
-	D[SI] = pivot;
-       	System.out.println(toString(D));
+	D[l] = pivot;
+
+	if (k < l){
+	    return select(D,k,low,l-1);
+	}
+	else if (k > l){
+	    return select(D,k,l+1,high);
+	}
+
+	System.out.println("\nAfter divisions:\n" + toString(D));
+	System.out.println("\nk is " + k + "\nThe kth smallest element is: ");
+	return pivot;
     }
 
     public String toString(int[] a){
@@ -41,8 +53,7 @@ public class Selection{
 	Selection s = new Selection();
 	int[] n = {10,2,5,1,16,44,70};
 	System.out.println("\nOriginal: \n" + s.toString(n));
-	System.out.println("\nDivided: ");
-	s.divide(n,0,6);	
+	System.out.println(s.select(n,6,0,6));	
     }
 
 }
